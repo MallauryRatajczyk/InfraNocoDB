7a61fb2cf3ece49cdaf5426d6530fa53cf4af6d5
Gorfilia <gorold@hotmail.fr>
2025-03-12 18:41:35 +0100

    FIX output #ROC-66

--- Storage/outputs.tf
+++ Storage/outputs.tf
@@ -1,3 +1,3 @@
 output "storage_instance_ips" {
-  value = google_sql_database_instance.postgres_instance.public_ip
+  value = google_sql_database_instance.postgres_instance.public_ip_address
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "ssh_key_file" {
-  type = string
+  type    = string
   default = ".ssh/id_rsa"
 }
 
