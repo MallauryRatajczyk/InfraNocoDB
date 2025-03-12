682d806645eab6a233ab51e356484c28b46f6f04
Gorfilia <gorold@hotmail.fr>
2025-03-12 18:33:17 +0100

    FIX output #ROC-66

--- Monitoring/outputs.tf
+++ Monitoring/outputs.tf
@@ -1,9 +1,9 @@
 
-output "instance_ip" {
+output "monitoring_instance_ip" {
   value       = google_compute_address.static_ip_monitoring.address
   description = "Adresse IP publique de la VM"
 }
 
-output "instance_name" {
+output "monitoring_instance_name" {
   value = google_compute_instance.prometheus_grafana_instance.name
 }
